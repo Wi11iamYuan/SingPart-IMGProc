a90b8a56cab6dd0257b03f717caf22b239c767bf
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2024-07-19 23:53:22 -0700

    working on block generation

--- bwconncomp_multi_iterative.py
+++ bwconncomp_multi_iterative.py
@@ -1,5 +1,7 @@
 #%%
 import numpy as np
+from math import ceil
+import sys
 
 import multiprocessing
 import threading
@@ -8,14 +10,116 @@ import matplotlib.pyplot as plt
 import pyvista as pv
 from constants import *
 
-import sys
-
 from img_conversion import img_toBW
 from bw_2d_gen import gen_RNG_2dBW
 from bw_3d_gen import gen_RNG_3dBW
 
 #%%
-def bwconncomp_iterative(BW = None, conn: int | None = None):
+def generate_2d_blocks(BW, cores):
+    width = BW.shape[0]
+    height = BW.shape[1]
+
+    #calculates the optimal number of blocks in the x and y directions
+    blocks_y = ceil(np.sqrt(cores * height / width))
+    blocks_x = ceil(cores / blocks_y)
+
+    #calculates the size of each block
+    block_width = width // blocks_x
+    block_height = height // blocks_y
+
+    #note the remainder pixels
+    remainder_x = width % blocks_x
+    remainder_y = height % blocks_y
+
+    blocks = []
+
+    x_start = 0
+    for i in range(blocks_x):
+        # Adjust block width if there are remainder pixels to distribute
+        current_width = block_width + (1 if i < remainder_x else 0)
+        y_start = 0
+        
+        for j in range(blocks_y):
+            # Adjust block height if there are remainder pixels to distribute
+            current_height = block_height + (1 if j < remainder_y else 0)
+            
+            # Create a block with its coordinates and size
+            block = {
+                'coord_start': (x_start, y_start),
+                'coord_end': (x_start + current_width, 
+                              y_start + current_height),
+                'label_range': (),
+            }
+            blocks.append(block)
+            
+            y_start += current_height
+        
+        x_start += current_width
+    
+    return blocks
+
+def generate_3d_blocks(BW, cores):
+    width = BW.shape[0]
+    height = BW.shape[1]
+    depth = BW.shape[2]
+
+    # Calculate the ratios
+    ratio_yx = height / width
+    ratio_zx = depth / width
+
+    # Calculate the number of blocks in each dimension
+    blocks_x = ceil((cores / (ratio_yx * ratio_zx)) ** (1/3))
+    blocks_y = ceil(blocks_x * ratio_yx)
+    blocks_z = ceil(blocks_x * ratio_zx)
+
+    # Calculate the size of each block
+    block_width = width // blocks_x
+    block_height = height // blocks_y
+    block_depth = depth // blocks_z
+
+    # Note the remainder pixels
+    remainder_x = width % blocks_x
+    remainder_y = height % blocks_y
+    remainder_z = depth % blocks_z
+
+    blocks = []
+
+    x_start = 0
+    for i in range(blocks_x):
+        # Adjust block width if there are remainder pixels to distribute
+        current_width = block_width + (1 if i < remainder_x else 0)
+        y_start = 0
+        
+        for j in range(blocks_y):
+            # Adjust block height if there are remainder pixels to distribute
+            current_height = block_height + (1 if j < remainder_y else 0)
+            z_start = 0
+
+            for k in range(blocks_z):
+                # Adjust block depth if there are remainder pixels to distribute
+                current_depth = block_depth + (1 if k < remainder_z else 0)
+            
+                # Create a block with its coordinates and size
+                block = {
+                    'coord_start': (x_start, y_start, z_start),
+                    'coord_end': (x_start + current_width, 
+                                  y_start + current_height, 
+                                  z_start + current_depth),
+                    'label_range': (),
+                }
+                blocks.append(block)
+
+                z_start += current_depth
+            
+            y_start += current_height
+        
+        x_start += current_width
+
+    return blocks
+
+
+#%%
+def bwconncomp_iterative(BW = None, conn: int | None = None, cores: int | None = None):
     #turns BW into a numpy array, then creates an empty track for the recursion later
     BW = np.asarray(BW)
 
@@ -57,11 +161,20 @@ def main():
 
     # image, component_indices = gen_RNG_3dBW(50, 50, 50, 5, 25, 50, conn6)
 
-    image, component_indices = BWTest.get_conn26_test(0)
+    # image, component_indices = BWTest.get_conn26_test(0)
 
-    CC = bwconncomp_iterative(image, 26)
+    # CC = bwconncomp_iterative(image, 26)
 
-    Tester.test_bwconncomp_match(CC, image, component_indices)
+    # Tester.test_bwconncomp_match(CC, image, component_indices)
+
+    # Example usage:
+    image = np.zeros((1000, 1500))  # Example image of size 1000x1500
+    blocks = generate_2d_blocks(image, cores=16)
+
+    # Print block information
+    for i, block in enumerate(blocks):
+        print(f"Block {i}: From ({block['x_start']}, {block['y_start']}) "
+            f"to ({block['x_end']}, {block['y_end']})")
 
     return 0
 
