bf8208025907ebdfe1b2e7267fc4f2445d4277d6
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2024-08-08 22:14:28 -0700

    vvery basic set up

--- regionprops.py
+++ regionprops.py
@@ -11,8 +11,24 @@ from bw_2d_gen import gen_RNG_2dBW
 
 
 
-def regionprops():
-    pass
+def regionprops(image, properties=None, output_format='struct'):
+    """
+    Accepts binary image (BW) or CC (connected components)
+
+    Accepts properties as a list of strings, if none, all properties are calculated
+    - Area, number of pixels in the region
+    - Centroid, center of mass of the region, 
+    - Bounding Box, smallest box containing the region
+
+    Specify output format
+    - Struct, array
+    - Table, table
+
+    """
+
+    region_props = {}
+
+    return region_props
 
 def main():
 
